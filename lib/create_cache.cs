8eb7293a5cf6e607da7c87280b24a99d33cd1c59
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-01 08:22:29 -0400

    create cache directory

---
+++ lib/create_cache.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ScienceBookLIB.cache
+{
+    class cache
+    {
+        String cache_dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\.sciencebook\\cache";
+
+        public void create(string cacheName)
+        {
+            if (!Directory.Exists(cache_dir)) { Directory.CreateDirectory(cache_dir); }
+        }
+    }
+
+    class RecentPageCache
+    {
+        public string PageName { get; set; }
+        public int LastModified { get; set; }
+        public string ModifiedIndex { get; set; }
+
+    }
+}
