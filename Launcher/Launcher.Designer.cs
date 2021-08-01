7ed12f9785ef3a0e769be053a80f3a141d4cbf3b
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-31 20:31:12 -0400

    main launcher base

--- Launcher/Launcher.Designer.cs
+++ Launcher/Launcher.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
-namespace ScienceBook
+namespace ScienceBook.Launcher
 {
-    partial class Form1
+    partial class Launcher
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,21 @@ namespace ScienceBook
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Launcher));
+            this.SuspendLayout();
+            // 
+            // Launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(930, 530);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Launcher";
+            this.Text = "Science Book - Launcher";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
--- Launcher/Launcher.cs
+++ Launcher/Launcher.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ScienceBook
+namespace ScienceBook.Launcher
 {
-    public partial class Form1 : Form
+    public partial class Launcher : Form
     {
-        public Form1()
+        public Launcher()
         {
             InitializeComponent();
         }
