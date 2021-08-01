744506424f8c747b2fbdfa2a1458efd0976d5f16
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-01 09:38:18 -0400

    read file content function

---
+++ lib/editor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ScienceBookLIB.editor
+{
+    class editor
+    {
+       public string GetContent(string path)
+        {
+            string path_name = path;
+            IEnumerable<string> lines = File.ReadLines(path_name);
+            string joint = String.Join(Environment.NewLine, lines);
+
+            return joint;
+        }
+    }
+}
